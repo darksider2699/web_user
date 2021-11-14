a91cffe2696c94fe8e643c15a701e03070ab855f
Anh Viet To <anh.viet.to@mgm-tp.com>
2021-11-14 19:14:15 +0700

    Add material UI

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
 
 const Customer = () => {
   return <div>Customers</div>;
