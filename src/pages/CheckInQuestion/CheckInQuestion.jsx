66698f9a25454956b7e2f1dc31268918b8f29954
Anh Viet To <anh.viet.to@mgm-tp.com>
2021-11-18 21:22:33 +0700

    checkin question

--- src/pages/CheckInQuestion/CheckInQuestion.jsx
+++ src/pages/CheckInQuestion/CheckInQuestion.jsx
@@ -1,34 +1,77 @@
-import React from "react";
-import { Box, Typography } from "@material-ui/core/";
+import React, { useState } from "react";
+import { Box, Typography, TextField } from "@material-ui/core/";
 import data from "../../assets/JsonData/question.json";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Radio from "@material-ui/core/Radio";
+
 const CheckInQuestion = ({ date, numberOfNegative, onClickRow }) => {
-  const employeeNumber = 100;
-  const handleOnClick = () => {
-    onClickRow(date);
-  };
+  const [answerOfQuestions, setAnswerOfQuestions] = useState([]);
+  const [question,setQuestion] = useState([]);
+  function handleChange(event) {
+    const questionId = event.target.name;
+    const answer = event.target.value;
+    let tempData = [...answerOfQuestions];
+    let indexOfAnswer = answerOfQuestions?.findIndex(
+      (element) => element.questionId === questionId + ""
+    );
+
+    indexOfAnswer > -1
+      ? (tempData[indexOfAnswer] = { questionId: questionId, answer: answer })
+      : tempData.push({ questionId: questionId, answer: answer });
+    setAnswerOfQuestions(tempData);
+    console.log(tempData);
+  }
   return (
-    <Box
-      onClick={handleOnClick}
-      borderRadius={10}
-      marginBottom={5}
-      style={{ cursor: "pointer" }}
-    >
+    <Box borderRadius={10} marginBottom={5} style={{ cursor: "pointer" }}>
       {data.map((index) => {
         return (
           <Box
-            onClick={handleOnClick}
+            key={index.id + "box"}
             bgcolor={"white"}
             borderRadius={10}
             marginBottom={5}
+            marginLeft={5}
             boxShadow={"rgba(149, 157, 165, 0.2) 0px 8px 24px"}
             justifyContent={"space-between"}
             display={"block"}
             width={"50%"}
             style={{ cursor: "pointer" }}
           >
-            <Typography style={{padding:'10px 10px 10px 10px'}}>{`${index.question}`}</Typography>
-            <Typography>{`${index.answer1}`}</Typography>
-            <Typography>{`${index.answer2}`}</Typography>
+            <TextField
+              defaultValue={index.question}
+              fullWidth
+              multiline
+              style={{padding:'10px 10px 10px 10px'}}
+              variant="outlined"
+
+            />
+
+            <RadioGroup
+              key={index.id}
+              aria-label="answer"
+              style={{paddingLeft:'20px'}}
+              name={index.id + ""}
+              value={
+                parseInt(
+                  answerOfQuestions?.find(
+                    (element) => element.questionId === index.id + ""
+                  )?.answer
+                ) || 2
+              }
+              onChange={handleChange}
+            >
+              {index.answers.map((answer) => {
+                return (
+                  <FormControlLabel
+                    key={index.id + "" + answer.answerId}
+                    value={answer.value}
+                    label={answer.label}
+                    control={<Radio />}
+                  />
+                );
+              })}
+            </RadioGroup>
           </Box>
         );
       })}
